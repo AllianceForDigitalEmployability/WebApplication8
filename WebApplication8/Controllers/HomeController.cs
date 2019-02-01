36107d2be6d537f94bd8a12137f2a2ad967e7a1a
davidoster <paspa@hotmail.com>
2019-02-01 09:33:22 +0200

    Connect to DB
    Send data from local database through ViewBag

--- WebApplication8/Controllers/HomeController.cs
+++ WebApplication8/Controllers/HomeController.cs
@@ -11,6 +11,8 @@ namespace WebApplication8.Controllers
 {
     public class HomeController : Controller
     {
+        private List<WebApplication7.Models.User> users = new List<WebApplication7.Models.User>();
+
         // GET: Home
         //public ActionResult Index()
         public string Index(string name)
@@ -18,32 +20,41 @@ namespace WebApplication8.Controllers
             string xyz = "List of Employees: <br />";
             xyz += name;
 
-            String connstring = "Data Source=\"ra1.anystream.eu,1010\";Initial Catalog=example_database;User ID=sa;Password=aDifficultPassword$";
-            using (SqlConnection connection = new SqlConnection(connstring))
-            {
-                connection.Open();
-                string sql = "SELECT Id, Name, Location FROM Employees;";
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            xyz += reader.GetInt32(0) + " " + reader.GetString(1) + " " + reader.GetString(2) + "<br />";
-                        }
-                    }
-                }
-
-            }
-
-
             string ss = "<!DOCTYPE html><html><body><h4>" + xyz + "</h4></body></html>";
             return ss; // View();
         }
 
         public ActionResult User()
         {
+            users = GetUsers();
+            //ViewBag.theUsers = users;
+            ViewBag.user = users[1].ToString();
             return View();
+        }
+
+        private List<WebApplication7.Models.User> GetUsers()
+        {
+            List<WebApplication7.Models.User> theUsers = new List<WebApplication7.Models.User>();
+
+            String connstring = "Data Source=\"ra1.anystream.eu,1010\";Initial Catalog=example_database;User ID=sa;Password=aDifficultPassword$";
+            String connstring2 = "Data Source=NBD-LW03\\SQLEXPRESS;Initial Catalog=eShop;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection connection = new SqlConnection(connstring2))
+            {
+                connection.Open();
+                string sql = "SELECT * FROM dbo.Users;";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            theUsers.Add(new WebApplication7.Models.User(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3)));
+                            //reader.GetInt32(0) + " " + reader.GetString(1) + " " + reader.GetString(2) + "<br />";
+                        }
+                    }
+                }
+            }
+            return theUsers;
         }
 
     }
--- WebApplication8/Models/User.cs
+++ WebApplication8/Models/User.cs
@@ -6,6 +6,24 @@
         public string name { get; set; }
         public string username { get; private set; }
         public string password { get; private set; }
+
+        public User(int id, string name, string username, string password)
+        {
+            this.id = id;
+            this.name = name;
+            this.username = username;
+            this.password = password;
+        }
+
+        public User GetUser()
+        {
+            return new User(this.id, this.name, this.username, this.password);
+        }
+
+        public string ToString()
+        {
+            return "Id: " + id + "\tName: " + name + "\tUsername: " + username + "\tPassword: " + password;
+        }
     }
 
 }
